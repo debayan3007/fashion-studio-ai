118c25ec349c254f72d0c28af51cfa3d8894fea3
Debayan S <debayansingha@gmail.com>
2025-11-07 13:30:37 +0530

    feat: Add ESLint configuration and scripts for linting and formatting TypeScript files

---
+++ backend/eslint.config.js
@@ -0,0 +1,30 @@
+import js from '@eslint/js';
+import eslintConfigPrettier from 'eslint-config-prettier';
+import tseslint from 'typescript-eslint';
+
+export default tseslint.config(
+  {
+    ignores: ['dist', 'coverage', 'node_modules'],
+  },
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    files: ['**/*.ts'],
+    languageOptions: {
+      parser: tseslint.parser,
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+    },
+    rules: {
+      '@typescript-eslint/no-unused-vars': [
+        'warn',
+        {
+          argsIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+        },
+      ],
+    },
+  },
+  eslintConfigPrettier,
+);
+
